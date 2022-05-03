cafee999025413ebd93b0b64c7a3df12bbf9309c
Arielrik <ariel.isaac.miranda@gmail.com>
2022-05-03 10:09:37 +0200

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace VeterinarioBasico
+{
+    class Conexion
+    {
+        public MySqlConnection conexion;
+
+        public Conexion()
+        {
+            conexion = new MySqlConnection("Server = 127.0.0.1; Database = veterinario; Uid = root; Pwd =; Port = 3306");
+        }
+
+        public string loginVeterinario(String DNI, String pass)
+        {
+            try
+            {
+                conexion.Open();
+                MySqlCommand consulta = new MySqlCommand("SELECT * FROM cliente WHERE dni = @DNI AND contrasena = @pass", conexion);
+                consulta.Parameters.AddWithValue("@DNI", DNI);
+                consulta.Parameters.AddWithValue("@pass", pass);
+                MySqlDataReader resultado = consulta.ExecuteReader();
+
+                if (resultado.Read())
+                {
+                    return resultado.GetString(0);
+                }
+
+                conexion.Close();
+                return "error de usuario/contraseña";
+            }
+            catch(MySqlException e)
+            {
+                return "error";
+                throw e;
+            }
+        }
+    }
+}
---
+++ VentanaLogin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VeterinarioBasico
+{
+    public partial class VentanaLogin : Form
+    {
+        Conexion conexion = new Conexion();
+
+        public VentanaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String resultadoConexion = conexion.loginVeterinario(textBoxDNI.Text, textBoxPass.Text);
+            MessageBox.Show(resultadoConexion);
+            //VentanaPrincipal v = new VentanaPrincipal();
+            //v.Show();
+        }
+    }
+}
